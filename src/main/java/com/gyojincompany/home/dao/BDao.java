b62d92ff96cd79b3ac304f38d445075f911403b4
gyojincompany <luckyred@gmail.com>
2023-02-15 14:54:09 +0900

    dataSource bean 추가, jdbc 의존설정 추가

--- src/main/java/com/gyojincompany/home/dao/BDao.java
+++ src/main/java/com/gyojincompany/home/dao/BDao.java
@@ -2,10 +2,26 @@ package com.gyojincompany.home.dao;
 
 import java.util.ArrayList;
 
+import javax.sql.DataSource;
+
 import com.gyojincompany.home.dto.BDto;
 
 public class BDao {
 	
+	DataSource dataSource;
+	
+	public BDao() {
+		super();
+		// TODO Auto-generated constructor stub
+		
+		
+		
+	}
+
+
+
+
+
 	public ArrayList<BDto> list() {
 		
 		ArrayList<BDto> dtos = new ArrayList<BDto>();
